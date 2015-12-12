e9ff99a5073de99b580cbf06f48f671f18ca2451
Developer <developer@MacBook-Pro-de-Developer.local>
2015-12-12 14:53:59 -0600

    Envío inicial del playground

---
+++ JuegoDeMemoria.playground/Contents.swift
@@ -0,0 +1,19 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var rango = 0...100
+for r in rango {
+    if r % 5 == 0 {
+        print("# \(r) Bingo!!!")
+    }
+    if r % 2 == 0 {
+        print("# \(r) par!!!")
+    }
+    if r % 2 != 0 {
+        print("# \(r) impar!!!")
+    }
+    if r >= 30 && r <= 40 {
+        print("# \(r) Viva Swift!!!")
+    }
+}
